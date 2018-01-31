baef7802389a422ae5b9e1c4acfbc1ae0c3ab8fd
Antoine PRUVOST <apruvost@e3r6p6.42.fr>
2018-01-31 15:36:39 +0100

    much better

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apruvost <apruvost@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 14:30:30 by apruvost          #+#    #+#             */
-/*   Updated: 2018/01/29 13:41:55 by apruvost         ###   ########.fr       */
+/*   Updated: 2018/01/31 14:39:44 by apruvost         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,11 @@ void	ft_init(t_win *hey)
 	hey->nbclicline = 0;
 	hey->height = WIN_HEIGHT;
 	hey->width = WIN_WIDTH;
+	hey->nbline = 0;
+	hey->nbrow = 0;
+	hey->originx = 300;
+	hey->originy = 300;
+	hey->len = 10;
 }
 
 int		main(int argc, char **argv)
@@ -48,14 +53,15 @@ int		main(int argc, char **argv)
 
 	if ((data = (t_win *)malloc(sizeof(t_win))) == NULL)
 		exit(0);
-	if (((fd = ft_getfile(argc, argv)) == -1))
-		exit(0);
 	ft_init(data);
+	if (((fd = ft_getfile(argc, argv, data)) == -1))
+		exit(0);
 	ft_create_map(fd, data);
 	data->mlxptr = mlx_init();
 	data->winptr = mlx_new_window(data->mlxptr, WIN_WIDTH, WIN_HEIGHT, "FdF");
 	mlx_mouse_hook(data->winptr, mouse_hook, (void *)data);
 	mlx_key_hook(data->winptr, key_hook, (void *)0);
+	ft_draw(data);
 	mlx_loop(data->mlxptr);
 	return (0);
 }
